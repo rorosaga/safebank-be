0b3606a3606326fd81771e093df1b70376bc2657
Maggie Chen <97855985+maggieyc@users.noreply.github.com>
2024-11-25 22:44:51 +0100

    try to fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import pytest
 from iebank_api.models import Account, User, Transaction
 from iebank_api import db, app
