e4958b598c09c7c11132ada52fc681dd64410c7a
rorosaga <rodrigo.sagastegui9@gmail.com>
2024-11-26 17:07:15 +0100

    sql fix

--- config.py
+++ config.py
@@ -15,15 +15,17 @@ class GithubCIConfig(Config):
     DEBUG = True
 
 class UATConfig(Config):
-    if os.getenv('ENV') == 'uat':
-        credential = DefaultAzureCredential()
-        SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-            dbuser=urllib.parse.quote(os.getenv('DBUSER')),
-            dbpass=credential.get_token(
-                'https://ossrdbms-aad.database.windows.net').token,
-            dbhost=os.getenv('DBHOST'),
-            dbname=os.getenv('DBNAME')
-        )
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/local.db'
+    DEBUG = True
+    # if os.getenv('ENV') == 'uat':
+    #     credential = DefaultAzureCredential()
+    #     SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    #         dbuser=urllib.parse.quote(os.getenv('DBUSER')),
+    #         dbpass=credential.get_token(
+    #             'https://ossrdbms-aad.database.windows.net').token,
+    #         dbhost=os.getenv('DBHOST'),
+    #         dbname=os.getenv('DBNAME')
+    #     )
 
 class DevelopmentConfig(Config):
     if os.getenv('ENV') == 'dev':
